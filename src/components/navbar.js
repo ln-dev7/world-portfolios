a3e4d556f0b8ab7c0380cd3ce72ead7f5941f614
ln-dev7 <leonelngoya@gmail.com>
2022-06-23 15:17:55 +0100

    Ajout de la barre de recherche

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,14 +5,28 @@ const NavBar = () => {
   return (
     <nav className="nav" id="#nav">
       <div className="nav-container">
-        <a href="https://cameroon-porfolios.vercel.app/" className="nav-container-logo">
+        <a
+          href="https://cameroon-porfolios.vercel.app/"
+          className="nav-container-logo"
+        >
           <img src="assets/images/logo.svg" alt="logo" />
         </a>
-        <a href="https://github.com/ln-dev7/cameroon-porfolios/blob/master/CONTRIBUTING.md" className="nav-container-link">
-          Add your portfolio
-        </a>
+        <div className="nav-container-menu">
+          <div className="nav-container-menu-search">
+            <input type="text" placeholder="Search a portfolio ..." />
+            <button>
+              <img src="assets/images/search-eye-line.svg" alt="search" />
+            </button>
+          </div>
+          <a
+            href="https://github.com/ln-dev7/cameroon-porfolios/blob/master/CONTRIBUTING.md"
+            className="nav-container-menu-link"
+          >
+            <span>Add your portfolio</span>
+            <span> + </span>
+          </a>
+        </div>
       </div>
-      
     </nav>
   );
 };
