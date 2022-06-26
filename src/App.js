c42e52ff145c1d2b5fb71a0cdf1300283c70f399
sanix-darker <s4nixd@gmail.com>
2022-06-26 03:22:21 +0200

    feat: add tag filtering

--- src/App.js
+++ src/App.js
@@ -6,48 +6,73 @@ import "./styles/App.scss";
 import dataOrder from "./data/dataOrder";
 import tags from "./data/tags";
 import Footer from "./components/Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const App = () => {
-  const [filteredData, setFilteredData] = useState(dataOrder);
+    const [filteredData, setFilteredData] = useState(dataOrder);
+    const [selectedTags, setSelectedTags] = useState(["all"]);
 
-  const changeFilterValue = (filterValue) => {
-    if (filterValue !== "")
-      setFilteredData(
-          dataOrder.filter((elem) => {
-              return elem.name
-                      .toLowerCase()
-                      .indexOf(filterValue.toLowerCase()) !== -1
-          })
-      );
-    else setFilteredData(dataOrder);
-  };
+    const setTag = (tag) => {
 
-  return (
-    <div className="App">
-      <a href="/">
-        <img
-          className="to-top"
-          src="assets/images/arrow-up-circle-fill.svg"
-          alt="arrow-up"
-        />
-      </a>
-      <NavBar onChangeValue={changeFilterValue} />
-      <Section />
-      <main className="main">
-        <div className="main-container">
-          <aside className="main-container-aside">
-            <TagList tags={tags}/>
-          </aside>
-          <div className="main-container-body">
-            <CardList filteredData={filteredData}/>
-          </div>
+        if (tag === "all")
+            setSelectedTags(["all"])
+        else if (selectedTags.indexOf(tag) !== -1)
+            setSelectedTags(selectedTags.filter((t) => t !== tag))
+        else
+            setSelectedTags([...selectedTags.filter((t) => t !== "all"), ...[tag]]);
+
+    }
+
+    const filterByName = (filterValue) => {
+
+        if (filterValue !== "")
+          setFilteredData(
+              dataOrder.filter((elem) => {
+                  return elem.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
+              })
+          );
+        else setFilteredData(dataOrder);
+    };
+
+    useEffect(() => {
+        if (selectedTags.indexOf("all") === -1 && selectedTags.length > 0){
+          setFilteredData(
+              dataOrder.filter((elem) => {
+                  for (var i=0; i < selectedTags.length; i++){
+                      return elem.tags.indexOf(selectedTags[i]) !== -1;
+                  }
+                  return false;
+              })
+          );
+        }
+        else setFilteredData(dataOrder);
+    }, [selectedTags])
+
+    return (
+        <div className="App">
+          <a href="/">
+            <img
+              className="to-top"
+              src="assets/images/arrow-up-circle-fill.svg"
+              alt="arrow-up"
+            />
+          </a>
+          <NavBar onChangeValue={filterByName} />
+          <Section />
+          <main className="main">
+            <div className="main-container">
+              <aside className="main-container-aside">
+                <TagList tags={tags} setTag={setTag} selectedTags={selectedTags} />
+              </aside>
+              <div className="main-container-body">
+                <CardList filteredData={filteredData}/>
+              </div>
+            </div>
+          </main>
+          <Footer />
         </div>
-      </main>
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default App;
