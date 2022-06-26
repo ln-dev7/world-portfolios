e981432b4940f7503d5deec119b9e6f0b065792f
sanix-darker <s4nixd@gmail.com>
2022-06-26 03:38:23 +0200

    feat: clean url
    - removing the http at the begining and the trailing slash at the end
    - shrink link for a maximum character (but keep the link working)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,20 @@
 import "./../styles/card.scss";
 
+
+const cleanUrl = (link) => {
+    return link
+        .replace(/^https?:\/\//, '')
+        .replace(/\/$/, '')
+        .slice(0, 29)
+            + (link.length > 29 ? "...": "");
+}
+
 const Card = ({ name, link, tags }) => (
     <div className="card">
       <div className="card-container">
         <h2>{name}</h2>
         <a href={link} target="_blank">
-          {link}
+          {cleanUrl(link)}
         </a>
       </div>
       <div className="card-tags">
