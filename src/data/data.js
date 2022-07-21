8c1ec7f19641332dad4bc321c021e23f4ee21983
dilane3 <komboudilane125@gmail.com>
2022-07-21 19:56:04 +0100

    Adding Dilane Kombou

--- src/data/data.js
+++ src/data/data.js
@@ -1,154 +1,154 @@
 const data = [
-	{
-		id: 1,
-		name: 'Leonel Ngoya',
-		link: 'https://lndev.me',
-		tags: ['FrontEnd', 'Web Integration'],
-	},
-	{
-		id: 2,
-		name: 'Arthur Monney ',
-		link: 'https://arthurmonney.me',
-		tags: ['FrontEnd', 'Design', 'BackEnd'],
-	},
-	{
-		id: 3,
-		name: 'Asam',
-		link: 'https://asam237.herokuapp.com',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 4,
-		name: 'Kemane Donfack',
-		link: 'https://kemanedonfack.000webhostapp.com/',
-		tags: ['FrontEnd', 'Mobile', 'Design', 'BackEnd'],
-	},
-	{
-		id: 5,
-		name: 'Valery Melou',
-		link: 'https://valerymelou.com/',
-		tags: ['FrontEnd', 'Backend', 'DevOps'],
-	},
-	{
-		id: 6,
-		name: 'Wilfried Djossi',
-		link: 'https://v1.la2spaille.vercel.app/',
-		tags: ['FrontEnd', 'Creative Dev'],
-	},
-	{
-		id: 7,
-		name: 'Adonis Simo',
-		link: 'https://adonissimo.com/',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 8,
-		name: 'Boris Gautier',
-		link: 'https://me.tbg.cm/',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 9,
-		name: 'Tekombo Gabriel',
-		link: 'https://gabrielthecode.com/',
-		tags: ['Mobile', 'FrontEnd'],
-	},
-	{
-		id: 10,
-		name: 'Ekim Kael',
-		link: 'https://ekimkael.github.io/',
-		tags: ['FrontEnd', 'Mobile', 'Design'],
-	},
-	{
-		id: 11,
-		name: 'Lemovou Ivan',
-		link: 'https://ivanlemovou.cm/',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 12,
-		name: 'Mystro Ken',
-		link: 'https://www.mystroken.com/',
-		tags: ['FrontEnd', 'Design', 'Creative Dev'],
-	},
-	{
-		id: 13,
-		name: 'Tchassi Jordan',
-		link: 'https://ijordan.tech/',
-		tags: ['FrontEnd', 'BackEnd'],
-	},
-	{
-		id: 14,
-		name: 'Real Jema',
-		link: 'http://realjema.com/',
-		tags: ['FrontEnd', 'BackEnd', 'Mobile', 'Design']
-	},
-	{
-		id: 15,
-		name: 'Aboubakar Sidik',
-		link: 'https://aboubak.art/',
-		tags: ['DevOps', 'FrontEnd', 'BackEnd', 'Mobile'],
-	},
-	{
-		id: 16,
-		name: 'Nenba Jonathan',
-		link: 'https://nenbajonathan.com/',
-		tags: ['DevOps', 'BackEnd'],
-	},
-	{
-		id: 17,
-		name: 'Franck Siyapze',
-		link: 'https://scorp-corporate.com/',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 18,
-		name: 'Jonathan Tchuente',
-		link: 'https://newgenius.tech/',
-		tags: ['Data scientist', 'BackEnd'],
-	},
-	{
-		id: 19,
-		name: 'Bmis arts',
-		link: 'http://bmis-arts.com/',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 20,
-		name: 'Donald Kuete',
-		link: 'https://resume.io/r/HaZAA1Sx9',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 21,
-		name: 'Djopa Wilfried',
-		link: 'https://djopa.fr',
-		tags: ['FrontEnd', 'Mobile', 'BackEnd'],
-	},
-	{
-		id: 22,
-		name: 'Roger Bentcha',
-		link: 'https://portfolio-tau-five-69.vercel.app',
-		tags: ['FrontEnd', 'BackEnd', 'Design']
-	},
-	{
-		id: 23,
-		name: 'Tondji Niat J. Lee',
-		link: 'https://ebirly.vercel.app',
-		tags: ['FrontEnd', 'BackEnd', 'Mobile', 'Design']
-	},
-	{
-		id: 24,
-		name: 'Marc Sitze',
-		link: 'https://theblackdev.herokuapp.com',
-		tags: ['FrontEnd', 'BackEnd', 'Mobile']
-	},
-	{
-​    id: 25,
-​    name: "Dilane Kombou",
-​    link: "https://dilane3.vercel.app",
-​    tags: ["FrontEnd", "Backend", "Mobile"]
-	},
-]
+  {
+    id: 1,
+    name: "Leonel Ngoya",
+    link: "https://lndev.me",
+    tags: ["FrontEnd", "Web Integration"],
+  },
+  {
+    id: 2,
+    name: "Arthur Monney ",
+    link: "https://arthurmonney.me",
+    tags: ["FrontEnd", "Design", "BackEnd"],
+  },
+  {
+    id: 3,
+    name: "Asam",
+    link: "https://asam237.herokuapp.com",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 4,
+    name: "Kemane Donfack",
+    link: "https://kemanedonfack.000webhostapp.com/",
+    tags: ["FrontEnd", "Mobile", "Design", "BackEnd"],
+  },
+  {
+    id: 5,
+    name: "Valery Melou",
+    link: "https://valerymelou.com/",
+    tags: ["FrontEnd", "Backend", "DevOps"],
+  },
+  {
+    id: 6,
+    name: "Wilfried Djossi",
+    link: "https://v1.la2spaille.vercel.app/",
+    tags: ["FrontEnd", "Creative Dev"],
+  },
+  {
+    id: 7,
+    name: "Adonis Simo",
+    link: "https://adonissimo.com/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 8,
+    name: "Boris Gautier",
+    link: "https://me.tbg.cm/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 9,
+    name: "Tekombo Gabriel",
+    link: "https://gabrielthecode.com/",
+    tags: ["Mobile", "FrontEnd"],
+  },
+  {
+    id: 10,
+    name: "Ekim Kael",
+    link: "https://ekimkael.github.io/",
+    tags: ["FrontEnd", "Mobile", "Design"],
+  },
+  {
+    id: 11,
+    name: "Lemovou Ivan",
+    link: "https://ivanlemovou.cm/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 12,
+    name: "Mystro Ken",
+    link: "https://www.mystroken.com/",
+    tags: ["FrontEnd", "Design", "Creative Dev"],
+  },
+  {
+    id: 13,
+    name: "Tchassi Jordan",
+    link: "https://ijordan.tech/",
+    tags: ["FrontEnd", "BackEnd"],
+  },
+  {
+    id: 14,
+    name: "Real Jema",
+    link: "http://realjema.com/",
+    tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
+  },
+  {
+    id: 15,
+    name: "Aboubakar Sidik",
+    link: "https://aboubak.art/",
+    tags: ["DevOps", "FrontEnd", "BackEnd", "Mobile"],
+  },
+  {
+    id: 16,
+    name: "Nenba Jonathan",
+    link: "https://nenbajonathan.com/",
+    tags: ["DevOps", "BackEnd"],
+  },
+  {
+    id: 17,
+    name: "Franck Siyapze",
+    link: "https://scorp-corporate.com/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 18,
+    name: "Jonathan Tchuente",
+    link: "https://newgenius.tech/",
+    tags: ["Data scientist", "BackEnd"],
+  },
+  {
+    id: 19,
+    name: "Bmis arts",
+    link: "http://bmis-arts.com/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 20,
+    name: "Donald Kuete",
+    link: "https://resume.io/r/HaZAA1Sx9",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 21,
+    name: "Djopa Wilfried",
+    link: "https://djopa.fr",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
+  },
+  {
+    id: 22,
+    name: "Roger Bentcha",
+    link: "https://portfolio-tau-five-69.vercel.app",
+    tags: ["FrontEnd", "BackEnd", "Design"],
+  },
+  {
+    id: 23,
+    name: "Tondji Niat J. Lee",
+    link: "https://ebirly.vercel.app",
+    tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
+  },
+  {
+    id: 24,
+    name: "Marc Sitze",
+    link: "https://theblackdev.herokuapp.com",
+    tags: ["FrontEnd", "BackEnd", "Mobile"],
+  },
+  {
+    id: 25,
+    name: "Dilane Kombou",
+    link: "https://dilane3.vercel.app",
+    tags: ["FrontEnd", "Backend", "Mobile"],
+  },
+];
 
 export default data;
