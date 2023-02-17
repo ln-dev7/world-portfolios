142a63fd8ae5e36f71b5c153dd3bdc5f67d3ad07
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2023-02-17 22:21:30 +0100

    add njeanclaude

--- src/data/data.js
+++ src/data/data.js
@@ -1,184 +1,190 @@
 const data = [
   {
     id: 1,
-    name: 'Leonel Ngoya',
-    link: 'https://lndev.me',
-    tags: ['FrontEnd', 'Web Integration']
+    name: "Leonel Ngoya",
+    link: "https://lndev.me",
+    tags: ["FrontEnd", "Web Integration"],
   },
   {
     id: 2,
-    name: 'Arthur Monney ',
-    link: 'https://arthurmonney.me',
-    tags: ['FrontEnd', 'Design', 'BackEnd']
+    name: "Arthur Monney ",
+    link: "https://arthurmonney.me",
+    tags: ["FrontEnd", "Design", "BackEnd"],
   },
   {
     id: 3,
-    name: 'Asam237',
-    link: 'https://asam-cmr.vercel.app/',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Asam237",
+    link: "https://asam-cmr.vercel.app/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 4,
-    name: 'Kemane Donfack',
-    link: 'https://kemanedonfack.herokuapp.com/',
-    tags: ['FrontEnd', 'Mobile', 'Design', 'BackEnd']
+    name: "Kemane Donfack",
+    link: "https://kemanedonfack.herokuapp.com/",
+    tags: ["FrontEnd", "Mobile", "Design", "BackEnd"],
   },
   {
     id: 5,
-    name: 'Valery Melou',
-    link: 'https://valerymelou.com/',
-    tags: ['FrontEnd', 'Backend', 'DevOps']
+    name: "Valery Melou",
+    link: "https://valerymelou.com/",
+    tags: ["FrontEnd", "Backend", "DevOps"],
   },
   {
     id: 6,
-    name: 'Wilfried Djossi',
-    link: 'https://v1.la2spaille.vercel.app/',
-    tags: ['FrontEnd', 'Creative Dev']
+    name: "Wilfried Djossi",
+    link: "https://v1.la2spaille.vercel.app/",
+    tags: ["FrontEnd", "Creative Dev"],
   },
   {
     id: 7,
-    name: 'Adonis Simo',
-    link: 'https://adonissimo.com/',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Adonis Simo",
+    link: "https://adonissimo.com/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 8,
-    name: 'Boris Gautier',
-    link: 'https://me.tbg.cm/',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Boris Gautier",
+    link: "https://me.tbg.cm/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 9,
-    name: 'Tekombo Gabriel',
-    link: 'https://gabrielthecode.com/',
-    tags: ['Mobile', 'FrontEnd']
+    name: "Tekombo Gabriel",
+    link: "https://gabrielthecode.com/",
+    tags: ["Mobile", "FrontEnd"],
   },
   {
     id: 10,
-    name: 'Ekim Kael',
-    link: 'https://ekimkael.github.io/',
-    tags: ['FrontEnd', 'Mobile', 'Design']
+    name: "Ekim Kael",
+    link: "https://ekimkael.github.io/",
+    tags: ["FrontEnd", "Mobile", "Design"],
   },
   {
     id: 11,
-    name: 'Lemovou Ivan',
-    link: 'https://ivanlemovou.cm/',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Lemovou Ivan",
+    link: "https://ivanlemovou.cm/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 12,
-    name: 'Mystro Ken',
-    link: 'https://www.mystroken.com/',
-    tags: ['FrontEnd', 'Design', 'Creative Dev']
+    name: "Mystro Ken",
+    link: "https://www.mystroken.com/",
+    tags: ["FrontEnd", "Design", "Creative Dev"],
   },
   {
     id: 13,
-    name: 'Tchassi Jordan',
-    link: 'https://ijordan.tech/',
-    tags: ['FrontEnd', 'BackEnd']
+    name: "Tchassi Jordan",
+    link: "https://ijordan.tech/",
+    tags: ["FrontEnd", "BackEnd"],
   },
   {
     id: 14,
-    name: 'Real Jema',
-    link: 'http://realjema.com/',
-    tags: ['FrontEnd', 'BackEnd', 'Mobile', 'Design']
+    name: "Real Jema",
+    link: "http://realjema.com/",
+    tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
   },
   {
     id: 15,
-    name: 'Aboubakar Sidik',
-    link: 'https://aboubak.art/',
-    tags: ['DevOps', 'FrontEnd', 'BackEnd', 'Mobile']
+    name: "Aboubakar Sidik",
+    link: "https://aboubak.art/",
+    tags: ["DevOps", "FrontEnd", "BackEnd", "Mobile"],
   },
   {
     id: 16,
-    name: 'Nenba Jonathan',
-    link: 'https://nenbajonathan.com/',
-    tags: ['DevOps', 'BackEnd']
+    name: "Nenba Jonathan",
+    link: "https://nenbajonathan.com/",
+    tags: ["DevOps", "BackEnd"],
   },
   {
     id: 17,
-    name: 'Franck Siyapze',
-    link: 'https://scorp-corporate.com/',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Franck Siyapze",
+    link: "https://scorp-corporate.com/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 18,
-    name: 'Jonathan Tchuente',
-    link: 'https://newgenius.tech/',
-    tags: ['Data scientist', 'BackEnd']
+    name: "Jonathan Tchuente",
+    link: "https://newgenius.tech/",
+    tags: ["Data scientist", "BackEnd"],
   },
   {
     id: 19,
-    name: 'Bmis arts',
-    link: 'http://bmis-arts.com/',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Bmis arts",
+    link: "http://bmis-arts.com/",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 20,
-    name: 'Donald Kuete',
-    link: 'https://resume.io/r/HaZAA1Sx9',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Donald Kuete",
+    link: "https://resume.io/r/HaZAA1Sx9",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 21,
-    name: 'Djopa Wilfried',
-    link: 'https://djopa.fr',
-    tags: ['FrontEnd', 'Mobile', 'BackEnd']
+    name: "Djopa Wilfried",
+    link: "https://djopa.fr",
+    tags: ["FrontEnd", "Mobile", "BackEnd"],
   },
   {
     id: 22,
-    name: 'Roger Bentcha',
-    link: 'https://portfolio-tau-five-69.vercel.app',
-    tags: ['FrontEnd', 'BackEnd', 'Design']
+    name: "Roger Bentcha",
+    link: "https://portfolio-tau-five-69.vercel.app",
+    tags: ["FrontEnd", "BackEnd", "Design"],
   },
   {
     id: 23,
-    name: 'Tondji Niat J. Lee',
-    link: 'https://ebirly.vercel.app',
-    tags: ['FrontEnd', 'BackEnd', 'Mobile', 'Design']
+    name: "Tondji Niat J. Lee",
+    link: "https://ebirly.vercel.app",
+    tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
   },
   {
     id: 24,
-    name: 'Marc Sitze',
-    link: 'https://theblackdev.herokuapp.com',
-    tags: ['FrontEnd', 'BackEnd', 'Mobile']
+    name: "Marc Sitze",
+    link: "https://theblackdev.herokuapp.com",
+    tags: ["FrontEnd", "BackEnd", "Mobile"],
   },
   {
     id: 25,
-    name: 'Dilane Kombou',
-    link: 'https://dilane3.vercel.app',
-    tags: ['FrontEnd', 'Backend', 'Mobile']
+    name: "Dilane Kombou",
+    link: "https://dilane3.vercel.app",
+    tags: ["FrontEnd", "Backend", "Mobile"],
   },
   {
     id: 26,
-    name: 'Bruce Guenkam',
-    link: 'https://guenkam.com',
-    tags: ['FrontEnd', 'Backend', 'Mobile']
+    name: "Bruce Guenkam",
+    link: "https://guenkam.com",
+    tags: ["FrontEnd", "Backend", "Mobile"],
   },
   {
     id: 27,
-    name: 'Fenyep Wangue',
-    link: 'https://fenyep-wangue.vercel.app',
-    tags: ['FrontEnd', 'Backend', 'Mobile']
+    name: "Fenyep Wangue",
+    link: "https://fenyep-wangue.vercel.app",
+    tags: ["FrontEnd", "Backend", "Mobile"],
   },
   {
     id: 28,
-    name: 'Baimam Boukar',
-    link: 'http://baimamboukar.me',
-    tags: ['Mobile', 'Cloud', 'Machine Learning']
+    name: "Baimam Boukar",
+    link: "http://baimamboukar.me",
+    tags: ["Mobile", "Cloud", "Machine Learning"],
   },
   {
     id: 29,
-    name: 'Boris Tchouanfe',
-    link: 'https://tchouanfeboris.netlify.app',
-    tags: ['Mobile', 'Frontend', 'Backend']
+    name: "Boris Tchouanfe",
+    link: "https://tchouanfeboris.netlify.app",
+    tags: ["Mobile", "Frontend", "Backend"],
   },
   {
     id: 30,
-    name: 'Hydil Aicard Sokeing',
-    link: 'https://elshaseries.icarpro.fr',
-    tags: ['Mobile', 'Backend']
-  }
-]
+    name: "Hydil Aicard Sokeing",
+    link: "https://elshaseries.icarpro.fr",
+    tags: ["Mobile", "Backend"],
+  },
+  {
+    id: 31,
+    name: "N Jean Claude ",
+    link: "portfolio-medjio.vercel.app",
+    tags: ["FrontEnd", "WebDesign", "GraphicDesign"],
+  },
+];
 
-export default data
+export default data;
