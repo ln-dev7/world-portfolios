be62be270a68aa6ae0f5e9f531ba6a2d87f85f3a
LN <62269693+ln-dev7@users.noreply.github.com>
2023-02-20 11:52:11 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -300,16 +300,6 @@ const data = [
     },
   },
   {
-    name: "Arnold Wabo",
-    link: "https://www.arnoldwabo.cm",
-    tags: ["Mobile", "Backend", "Sotware engineering"],
-    socials: {
-      twitter: "arnold_wabo",
-      github: "venusdjinni",
-      linkedin: ""
-    }
-  },
-  {
     name: "Audran Wolfhards",
     link: "https://audran-wol.github.io/Audran/",
     tags: ["Frontend", "Backend", "Design", "Data Analyst", "Content Creator"],
@@ -319,6 +309,16 @@ const data = [
         linkedin: "𝐀𝐔𝐃𝐑𝐀𝐍-𝐖𝐎𝐋𝐅𝐇𝐀𝐑𝐃𝐒-7aab3321b",
       },
 },
+{
+    name: "Arnold Wabo",
+    link: "https://www.arnoldwabo.cm",
+    tags: ["Mobile", "Backend"],
+    socials: {
+      twitter: "arnold_wabo",
+      github: "venusdjinni",
+      linkedin: ""
+    }
+  },
             
 ];
 
