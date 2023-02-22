ab784b97f59ede52888a2f907ef3ecc8623f040a
Tchoupe <tchoupemartinjunior@gmail.com>
2023-02-22 15:12:06 +0100

    Martin Jr Tchoupe portfolio added

--- src/data/data.js
+++ src/data/data.js
@@ -1,356 +1,365 @@
-const data = [
-  {
-    name: "Leonel Ngoya",
-    link: "https://lndev.me",
-    tags: ["FrontEnd", "Creative dev"],
-    socials: {
-      twitter: "ln_dev7",
-      github: "ln-dev7",
-      linkedin: "lndev",
+const data = [{
+        name: "Leonel Ngoya",
+        link: "https://lndev.me",
+        tags: ["FrontEnd", "Creative dev"],
+        socials: {
+            twitter: "ln_dev7",
+            github: "ln-dev7",
+            linkedin: "lndev",
+        },
     },
-  },
-  {
-    name: "Arthur Monney",
-    link: "https://arthurmonney.me",
-    tags: ["FrontEnd", "Design", "BackEnd"],
-    socials: {
-      twitter: "monneyarthur",
-      github: "mckenziearts",
-      linkedin: "arthurmonney",
+    {
+        name: "Arthur Monney",
+        link: "https://arthurmonney.me",
+        tags: ["FrontEnd", "Design", "BackEnd"],
+        socials: {
+            twitter: "monneyarthur",
+            github: "mckenziearts",
+            linkedin: "arthurmonney",
+        },
     },
-  },
-  {
-    name: "Asam237",
-    link: "https://asam-cmr.vercel.app/",
-    tags: ["FrontEnd", "Mobile", "BackEnd"],
-    socials: {
-      twitter: "asam_237",
-      github: "Asam237",
-      linkedin: "abba-sali-aboubakar-mamate",
+    {
+        name: "Asam237",
+        link: "https://asam-cmr.vercel.app/",
+        tags: ["FrontEnd", "Mobile", "BackEnd"],
+        socials: {
+            twitter: "asam_237",
+            github: "Asam237",
+            linkedin: "abba-sali-aboubakar-mamate",
+        },
     },
-  },
-  {
-    name: "Valery Melou",
-    link: "https://valerymelou.com/",
-    tags: ["FrontEnd", "Backend", "DevOps"],
-    socials: {
-      twitter: "valerymelou",
-      github: "valerymelou",
-      linkedin: "valerymelou",
+    {
+        name: "Valery Melou",
+        link: "https://valerymelou.com/",
+        tags: ["FrontEnd", "Backend", "DevOps"],
+        socials: {
+            twitter: "valerymelou",
+            github: "valerymelou",
+            linkedin: "valerymelou",
+        },
     },
-  },
-  {
-    name: "Wilfried Djossi",
-    link: "https://la2spaille.studio/",
-    tags: ["FrontEnd", "Creative Dev"],
-    socials: {
-      twitter: "la2spaille",
-      github: "la2spaille",
-      linkedin: "wilfried-djossi-92b2b222b",
+    {
+        name: "Wilfried Djossi",
+        link: "https://la2spaille.studio/",
+        tags: ["FrontEnd", "Creative Dev"],
+        socials: {
+            twitter: "la2spaille",
+            github: "la2spaille",
+            linkedin: "wilfried-djossi-92b2b222b",
+        },
     },
-  },
-  {
-    name: "Adonis Simo",
-    link: "https://blog.adonissimo.com/",
-    tags: ["FrontEnd", "Mobile", "BackEnd"],
-    socials: {
-      twitter: "adonis__simo",
-      github: "simo97",
-      linkedin: "adonis-simo",
+    {
+        name: "Adonis Simo",
+        link: "https://blog.adonissimo.com/",
+        tags: ["FrontEnd", "Mobile", "BackEnd"],
+        socials: {
+            twitter: "adonis__simo",
+            github: "simo97",
+            linkedin: "adonis-simo",
+        },
     },
-  },
-  {
-    name: "Boris Gautier",
-    link: "https://borisgauty.com",
-    tags: ["FrontEnd", "Mobile", "BackEnd"],
-    socials: {
-      twitter: "Boris_Gauty",
-      github: "BorisGautier",
-      linkedin: "boris-gautier-tchoukouaha-420262143",
+    {
+        name: "Boris Gautier",
+        link: "https://borisgauty.com",
+        tags: ["FrontEnd", "Mobile", "BackEnd"],
+        socials: {
+            twitter: "Boris_Gauty",
+            github: "BorisGautier",
+            linkedin: "boris-gautier-tchoukouaha-420262143",
+        },
     },
-  },
-  {
-    name: "Tekombo Gabriel",
-    link: "https://gabrielthecode.com/",
-    tags: ["Mobile", "FrontEnd"],
-    socials: {
-      twitter: "gabriel_thecode",
-      github: "gabriel-thecode",
-      linkedin: "tekombo-gabriel",
+    {
+        name: "Tekombo Gabriel",
+        link: "https://gabrielthecode.com/",
+        tags: ["Mobile", "FrontEnd"],
+        socials: {
+            twitter: "gabriel_thecode",
+            github: "gabriel-thecode",
+            linkedin: "tekombo-gabriel",
+        },
     },
-  },
-  {
-    name: "Ekim Kael",
-    link: "https://ekimkael.github.io/",
-    tags: ["FrontEnd", "Mobile", "Design"],
-    socials: {
-      twitter: "ekimkael",
-      github: "ekimkael",
-      linkedin: "michelmbili",
+    {
+        name: "Ekim Kael",
+        link: "https://ekimkael.github.io/",
+        tags: ["FrontEnd", "Mobile", "Design"],
+        socials: {
+            twitter: "ekimkael",
+            github: "ekimkael",
+            linkedin: "michelmbili",
+        },
     },
-  },
-  {
-    name: "Lemovou Ivan",
-    link: "https://ivanlemovou.cm/",
-    tags: ["FrontEnd", "Mobile", "BackEnd"],
-    socials: {
-      twitter: "sherlock2045",
-      github: "SherlockHolmes2045",
-      linkedin: "ivan-lemovou-98a585171",
+    {
+        name: "Lemovou Ivan",
+        link: "https://ivanlemovou.cm/",
+        tags: ["FrontEnd", "Mobile", "BackEnd"],
+        socials: {
+            twitter: "sherlock2045",
+            github: "SherlockHolmes2045",
+            linkedin: "ivan-lemovou-98a585171",
+        },
     },
-  },
-  {
-    name: "Mystro Ken",
-    link: "https://www.mystroken.com/",
-    tags: ["FrontEnd", "Design", "Creative Dev"],
-    socials: {
-      twitter: "mystroken",
-      github: "mystroken",
-      linkedin: "emmanuel-kwene",
+    {
+        name: "Mystro Ken",
+        link: "https://www.mystroken.com/",
+        tags: ["FrontEnd", "Design", "Creative Dev"],
+        socials: {
+            twitter: "mystroken",
+            github: "mystroken",
+            linkedin: "emmanuel-kwene",
+        },
     },
-  },
-  {
-    name: "Real Jema",
-    link: "https://realjema.com/",
-    tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
-    socials: {
-      twitter: "realjema_",
-      github: "realJema",
-      linkedin: "realjema",
+    {
+        name: "Real Jema",
+        link: "https://realjema.com/",
+        tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
+        socials: {
+            twitter: "realjema_",
+            github: "realJema",
+            linkedin: "realjema",
+        },
     },
-  },
-  {
-    name: "Aboubakar Sidik",
-    link: "https://aboubak.art/",
-    tags: ["DevOps", "FrontEnd", "BackEnd", "Mobile"],
-    socials: {
-      twitter: "sidikfaha",
-      github: "sidikfaha",
-      linkedin: "fahasidik",
+    {
+        name: "Aboubakar Sidik",
+        link: "https://aboubak.art/",
+        tags: ["DevOps", "FrontEnd", "BackEnd", "Mobile"],
+        socials: {
+            twitter: "sidikfaha",
+            github: "sidikfaha",
+            linkedin: "fahasidik",
+        },
     },
-  },
-  {
-    name: "Nenba Jonathan",
-    link: "https://nenbajonathan.com/",
-    tags: ["DevOps", "BackEnd", "FrontEnd", "Web3"],
-    socials: {
-      twitter: "nejos97",
-      github: "nejos97",
-      linkedin: "jnenba",
+    {
+        name: "Nenba Jonathan",
+        link: "https://nenbajonathan.com/",
+        tags: ["DevOps", "BackEnd", "FrontEnd", "Web3"],
+        socials: {
+            twitter: "nejos97",
+            github: "nejos97",
+            linkedin: "jnenba",
+        },
     },
-  },
-  {
-    name: "Franck Siyapze",
-    link: "https://scorp-corporate.com/",
-    tags: ["FrontEnd", "Mobile", "BackEnd"],
-    socials: {
-      twitter: "thehuck_s",
-      github: "FranckSiyapze",
-      linkedin: "franck-dabryn-siyapze-3276b4120",
+    {
+        name: "Franck Siyapze",
+        link: "https://scorp-corporate.com/",
+        tags: ["FrontEnd", "Mobile", "BackEnd"],
+        socials: {
+            twitter: "thehuck_s",
+            github: "FranckSiyapze",
+            linkedin: "franck-dabryn-siyapze-3276b4120",
+        },
     },
-  },
-  {
-    name: "Jonathan Tchuente",
-    link: "https://newgenius.tech/",
-    tags: ["Data scientist", "BackEnd"],
-    socials: {
-      twitter: "new1genius",
-      github: "geek3000",
-      linkedin: "newgenius",
+    {
+        name: "Jonathan Tchuente",
+        link: "https://newgenius.tech/",
+        tags: ["Data scientist", "BackEnd"],
+        socials: {
+            twitter: "new1genius",
+            github: "geek3000",
+            linkedin: "newgenius",
+        },
     },
-  },
-  {
-    name: "Djopa Wilfried",
-    link: "https://djopa.fr",
-    tags: ["FrontEnd", "Mobile", "BackEnd"],
-    socials: {
-      twitter: "willy_djopa",
-      github: "wdjopa",
-      linkedin: "wilfried-djopa",
+    {
+        name: "Djopa Wilfried",
+        link: "https://djopa.fr",
+        tags: ["FrontEnd", "Mobile", "BackEnd"],
+        socials: {
+            twitter: "willy_djopa",
+            github: "wdjopa",
+            linkedin: "wilfried-djopa",
+        },
     },
-  },
-  {
-    name: "Roger Bentcha",
-    link: "https://rogerbentcha.fr/",
-    tags: ["FrontEnd", "BackEnd", "Design"],
-    socials: {
-      twitter: "RBentcha",
-      github: "Carter2307",
-      linkedin: "roger-bentcha-280802195",
+    {
+        name: "Roger Bentcha",
+        link: "https://rogerbentcha.fr/",
+        tags: ["FrontEnd", "BackEnd", "Design"],
+        socials: {
+            twitter: "RBentcha",
+            github: "Carter2307",
+            linkedin: "roger-bentcha-280802195",
+        },
     },
-  },
-  {
-    name: "Tondji Niat J. Lee",
-    link: "https://ebirly.vercel.app",
-    tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
-    socials: {
-      twitter: "Leesan30Lee",
-      github: "Geekles007",
-      linkedin: "",
+    {
+        name: "Tondji Niat J. Lee",
+        link: "https://ebirly.vercel.app",
+        tags: ["FrontEnd", "BackEnd", "Mobile", "Design"],
+        socials: {
+            twitter: "Leesan30Lee",
+            github: "Geekles007",
+            linkedin: "",
+        },
     },
-  },
-  {
-    name: "Dilane Kombou",
-    link: "https://dilane3.com",
-    tags: ["FrontEnd", "Backend", "Mobile"],
-    socials: {
-      twitter: "dilanekombou",
-      github: "dilane3",
-      linkedin: "dilane-kombou-6824b2207",
+    {
+        name: "Dilane Kombou",
+        link: "https://dilane3.com",
+        tags: ["FrontEnd", "Backend", "Mobile"],
+        socials: {
+            twitter: "dilanekombou",
+            github: "dilane3",
+            linkedin: "dilane-kombou-6824b2207",
+        },
     },
-  },
-  {
-    name: "Bruce Guenkam",
-    link: "https://guenkam.com",
-    tags: ["FrontEnd", "Backend", "Mobile"],
-    socials: {
-      twitter: "SandersLeGeek",
-      github: "sand97",
-      linkedin: "bruce-guenkam",
+    {
+        name: "Bruce Guenkam",
+        link: "https://guenkam.com",
+        tags: ["FrontEnd", "Backend", "Mobile"],
+        socials: {
+            twitter: "SandersLeGeek",
+            github: "sand97",
+            linkedin: "bruce-guenkam",
+        },
     },
-  },
-  {
-    name: "Fenyep Wangue",
-    link: "https://fenyep-wangue.vercel.app",
-    tags: ["FrontEnd", "Backend", "Mobile"],
-    socials: {
-      twitter: "FenyepW",
-      github: "Fenyep",
-      linkedin: "wangue-fenyep-631096193",
+    {
+        name: "Fenyep Wangue",
+        link: "https://fenyep-wangue.vercel.app",
+        tags: ["FrontEnd", "Backend", "Mobile"],
+        socials: {
+            twitter: "FenyepW",
+            github: "Fenyep",
+            linkedin: "wangue-fenyep-631096193",
+        },
     },
-  },
-  {
-    name: "Boris Tchouanfe",
-    link: "https://tchouanfeboris.netlify.app",
-    tags: ["Mobile", "Frontend", "Backend"],
-    socials: {
-      twitter: "BTchouanfe",
-      github: "",
-      linkedin: "tchouanfe-boris-327824218",
+    {
+        name: "Boris Tchouanfe",
+        link: "https://tchouanfeboris.netlify.app",
+        tags: ["Mobile", "Frontend", "Backend"],
+        socials: {
+            twitter: "BTchouanfe",
+            github: "",
+            linkedin: "tchouanfe-boris-327824218",
+        },
     },
-  },
-  {
-    name: "Hydil Aicard Sokeing",
-    link: "https://elshaseries.icarpro.fr",
-    tags: ["Mobile", "Backend"],
-    socials: {
-      twitter: "elshaseries",
-      github: "hydilois",
-      linkedin: "hydil-aicard-sokeing-b0b74310b",
+    {
+        name: "Hydil Aicard Sokeing",
+        link: "https://elshaseries.icarpro.fr",
+        tags: ["Mobile", "Backend"],
+        socials: {
+            twitter: "elshaseries",
+            github: "hydilois",
+            linkedin: "hydil-aicard-sokeing-b0b74310b",
+        },
     },
-  },
-  {
-    name: "Kengne Jeffrey",
-    link: "https://jeff.hooreo.com",
-    tags: ["FrontEnd", "Backend", "Mobile"],
-    socials: {
-      twitter: "JeffreyKen9",
-      github: "jeffrey0606",
-      linkedin: "jeffrey-kengne-142153196",
+    {
+        name: "Kengne Jeffrey",
+        link: "https://jeff.hooreo.com",
+        tags: ["FrontEnd", "Backend", "Mobile"],
+        socials: {
+            twitter: "JeffreyKen9",
+            github: "jeffrey0606",
+            linkedin: "jeffrey-kengne-142153196",
+        },
     },
-  },
-  {
-    name: "N Jean Claude",
-    link: "https://portfolio-medjio.vercel.app",
-    tags: ["FrontEnd", "Design"],
-    socials: {
-      twitter: "",
-      github: "",
-      linkedin: "jean-claude-nganmedjio-603995a1",
+    {
+        name: "N Jean Claude",
+        link: "https://portfolio-medjio.vercel.app",
+        tags: ["FrontEnd", "Design"],
+        socials: {
+            twitter: "",
+            github: "",
+            linkedin: "jean-claude-nganmedjio-603995a1",
+        },
     },
-  },
-  {
-    name: "Bleriot Noguia",
-    link: "https://bleriotnoguia.com",
-    tags: ["Frontend", "Mobile", "Backend"],
-    socials: {
-      twitter: "bleriotnoguia",
-      github: "bleriotnoguia",
-      linkedin: "bleriotnoguia",
+    {
+        name: "Bleriot Noguia",
+        link: "https://bleriotnoguia.com",
+        tags: ["Frontend", "Mobile", "Backend"],
+        socials: {
+            twitter: "bleriotnoguia",
+            github: "bleriotnoguia",
+            linkedin: "bleriotnoguia",
+        },
     },
-  },
-  {
-    name: "Cedrick Kouekam",
-    link: "https://cedrick-kouekam-portfolio.web.app/",
-    tags: ["Frontend"],
-    socials: {
-      twitter: "cedrickkouek",
-      github: "cedrickkouek",
-      linkedin: "cedrick-kouek-320a06178",
+    {
+        name: "Cedrick Kouekam",
+        link: "https://cedrick-kouekam-portfolio.web.app/",
+        tags: ["Frontend"],
+        socials: {
+            twitter: "cedrickkouek",
+            github: "cedrickkouek",
+            linkedin: "cedrick-kouek-320a06178",
+        },
     },
-  },
-  {
-    name: "David Ouatedem",
-    link: "https://davdev.netlify.app/",
-    tags: ["Frontend"],
-    socials: {
-      twitter: "davdev1400",
-      github: "davdev14",
-      linkedin: "david-ouatedem-077985253",
+    {
+        name: "David Ouatedem",
+        link: "https://davdev.netlify.app/",
+        tags: ["Frontend"],
+        socials: {
+            twitter: "davdev1400",
+            github: "davdev14",
+            linkedin: "david-ouatedem-077985253",
+        },
     },
-  },
-  {
-    name: "Schuame Alexandre",
-    link: "https://schuamealexandre.com/",
-    tags: ["Frontend", "Backend"],
-    socials: {
-      twitter: "motomoto_sc",
-      github: "alessandrini11",
-      linkedin: "",
+    {
+        name: "Schuame Alexandre",
+        link: "https://schuamealexandre.com/",
+        tags: ["Frontend", "Backend"],
+        socials: {
+            twitter: "motomoto_sc",
+            github: "alessandrini11",
+            linkedin: "",
+        },
     },
-  },
-  {
-    name: "Audran Wolfhards",
-    link: "https://audran-wol.github.io/Audran/",
-    tags: ["Frontend", "Backend", "Design", "Data Analyst", "Content Creator"],
-    socials: {
-      twitter: "WolfhardsA",
-      github: "Audran-wol",
-      linkedin: "𝐀𝐔𝐃𝐑𝐀𝐍-𝐖𝐎𝐋𝐅𝐇𝐀𝐑𝐃𝐒-7aab3321b",
+    {
+        name: "Audran Wolfhards",
+        link: "https://audran-wol.github.io/Audran/",
+        tags: ["Frontend", "Backend", "Design", "Data Analyst", "Content Creator"],
+        socials: {
+            twitter: "WolfhardsA",
+            github: "Audran-wol",
+            linkedin: "𝐀𝐔𝐃𝐑𝐀𝐍-𝐖𝐎𝐋𝐅𝐇𝐀𝐑𝐃𝐒-7aab3321b",
+        },
     },
-  },
-  {
-    name: "Arnold Wabo",
-    link: "https://www.arnoldwabo.cm",
-    tags: ["Mobile", "Backend"],
-    socials: {
-      twitter: "arnold_wabo",
-      github: "venusdjinni",
-      linkedin: "",
+    {
+        name: "Arnold Wabo",
+        link: "https://www.arnoldwabo.cm",
+        tags: ["Mobile", "Backend"],
+        socials: {
+            twitter: "arnold_wabo",
+            github: "venusdjinni",
+            linkedin: "",
+        },
     },
-  },
-  {
-    name: "Kopper Ngounou",
-    link: "https://ngounoukopper.me/",
-    tags: ["Cybersecurity", "Design", "Cloud Computing"],
-    socials: {
-      twitter: "ngounoukopper",
-      github: "ngounoukopper",
-      linkedin: "ngounoukopper",
+    {
+        name: "Kopper Ngounou",
+        link: "https://ngounoukopper.me/",
+        tags: ["Cybersecurity", "Design", "Cloud Computing"],
+        socials: {
+            twitter: "ngounoukopper",
+            github: "ngounoukopper",
+            linkedin: "ngounoukopper",
+        },
     },
-  },
-  {
-    name: "Yvan Nguekam",
-    link: "https://main--yvan-nguekam.netlify.app/",
-    tags: ["Frontend", "Backend", "Design"],
-    socials: {
-      twitter: "YvanNguekam",
-      github: "yvan-nguekam",
-      linkedin: "yvan-nguekam-7b7762193",
+    {
+        name: "Yvan Nguekam",
+        link: "https://main--yvan-nguekam.netlify.app/",
+        tags: ["Frontend", "Backend", "Design"],
+        socials: {
+            twitter: "YvanNguekam",
+            github: "yvan-nguekam",
+            linkedin: "yvan-nguekam-7b7762193",
+        },
     },
-  },
-  
-  {
-    name: "Mboh Bless Pearl N",
-    link: "https://ashy-bush-0b6a78203.2.azurestaticapps.net/",
-    tags: ["Mobile", "Backend", "cloud"],
-    socials: {
-      twitter: "Mboh_Bless",
-      github: "MbohBless",
-      linkedin: "mboh-bless-pearl-a2884a16b",
+
+    {
+        name: "Mboh Bless Pearl N",
+        link: "https://ashy-bush-0b6a78203.2.azurestaticapps.net/",
+        tags: ["Mobile", "Backend", "cloud"],
+        socials: {
+            twitter: "Mboh_Bless",
+            github: "MbohBless",
+            linkedin: "mboh-bless-pearl-a2884a16b",
+        },
     },
-  },
-  
+    {
+        name: "Martin Junior Tchoupe",
+        link: "https://tchoupemartinjunior.github.io/",
+        tags: ["FullStack", "Web/Mobile", "UI UX design", "Machine Learning", "IoT", "Embedded software"],
+        socials: {
+            twitter: "",
+            github: "tchoupemartinjunior",
+            linkedin: "martin-junior-tchoupe-b75b24203",
+        },
+    },
+
 ];
 
 export default data;
