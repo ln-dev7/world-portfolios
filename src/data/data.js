4839ce2712e520ee4d2c3b7d45a33f640282d6ba
Alfred MOUELLE <alfredmouelle@gmail.com>
2023-05-30 10:10:43 +0100

    added portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -440,6 +440,16 @@ const data = [
       linkedin: "mael-toukap-4577041a4",
     },
   },
+  {
+    name: "Alfred Mouelle",
+    link: "https://alfred-mouelle.zd.fr",
+    tags: ["Backend", "FrontEnd", "Design"],
+    socials: {
+      twitter: "kali47_",
+      github: "alfredmouelle",
+      linkedin: "alfred-mouelle-72a976228",
+    },
+  },
 ];
 
 export default data;
