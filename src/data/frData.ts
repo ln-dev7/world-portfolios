e04935a3ff5823ed75f0a2c71d0ecce097c7ff5a
LN <leonelngoya@gmail.com>
2023-07-16 11:44:56 +0100

    add arthur poulain portfolio

--- src/data/frData.ts
+++ src/data/frData.ts
@@ -1,5 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
-const frData: CountryData[] = [];
+const frData: CountryData[] = [
+  {
+    name: "Arthur Poulain",
+    link: "https://arthur-poulain.com",
+    tags: ["FrontEnd", "Design"],
+    socials: {
+      github: "arthurpdev",
+      twitter: "arthurpoln",
+      linkedin: "",
+    },
+  },
+];
 
 export default frData;
