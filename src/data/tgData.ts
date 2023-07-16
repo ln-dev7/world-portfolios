f75914e0342c13af69c6f2818922d5ba8512e245
Gauthier Eholoum <thiergau691@gmail.com>
2023-07-16 10:50:46 +0000

    chore: add gausoft profile

--- src/data/tgData.ts
+++ src/data/tgData.ts
@@ -1,5 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
-const tgData: CountryData[] = [];
+const tgData: CountryData[] = [
+  {
+    name: "Gautier EHOLOUM",
+    link: "https://gausoft.dev",
+    tags: ["Backend", "Mobile", "Data", "Open Source"],
+    socials: {
+      twitter: "gausoft_",
+      github: "gausoft",
+      linkedin: "https://linkedin.com/in/gauthier-eholoum",
+    },
+  },
+];
 
 export default tgData;
