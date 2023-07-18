2a798194aa63fd90f5abd74dd9016fbd1c022323
LN <leonelngoya@gmail.com>
2023-07-18 19:33:41 +0100

    add adams aime desire

--- src/data/ciData.ts
+++ src/data/ciData.ts
@@ -1,5 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
-const ciData: CountryData[] = [];
+const ciData: CountryData[] = [
+  {
+    name: "Adams Aimé-Désiré",
+    link: "https://aimedesire.com",
+    tags: ["Frontend", "Design", "Backend"],
+    socials: {
+      twitter: "dams9ix",
+      linkedin: "adamsaimedesireofficial",
+      github: "devdams",
+    },
+  },
+];
 
 export default ciData;
