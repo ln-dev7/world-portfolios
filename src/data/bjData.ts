75a85c47ad80bd3ea8797b08f9561cffda918fe6
LN <leonelngoya@gmail.com>
2023-07-23 22:01:58 +0100

    Axel VISSOH

--- src/data/bjData.ts
+++ src/data/bjData.ts
@@ -1,5 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
-const bjData: CountryData[] = [];
+const bjData: CountryData[] = [
+  {
+    name: "Axel VISSOH",
+    link: "https://storage.googleapis.com/portfolio-axelvissoh/personalWebsite/index.html",
+    tags: ["Fullstack", "QA Tester"],
+    socials: {
+      twitter: "",
+      github: "axelVISSOH",
+      linkedin: "inaxelvissoh04",
+    },
+  },
+];
 
 export default bjData;
