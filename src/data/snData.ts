d6bd473952efe61a28d1219fb42bde3bf4fbc104
ln-dev7 <leonelngoya@gmail.com>
2023-10-04 19:41:34 +0800

    Add Abdoul Oumar SY

--- src/data/snData.ts
+++ src/data/snData.ts
@@ -1,5 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
-const snData: CountryData[] = [];
+const snData: CountryData[] = [
+  {
+    name: "Abdoul Oumar SY",
+    link: "https://abdouloumar.tech/",
+    tags: ["FrontEnd"],
+    socials: {
+      github: "x__abdoul",
+      twitter: "Abdoulomr",
+      linkedin: "abdoul-oumar-sy-b34443169",
+    },
+  },
+];
 
 export default snData;
