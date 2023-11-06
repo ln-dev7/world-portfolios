f984d720832fa35235a717a7ab5e2481a3605024
LN <leonelngoya@gmail.com>
2023-11-06 17:51:58 +0100

    use next font

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,13 +1,11 @@
 import { Inter, Overpass } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-
-// export const inter = Inter({
-//     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-//     subsets: ["latin"],
-//     display: 'swap',
-//      variable: '--font-inter',
-//   });
+export const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const overpass = Overpass({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
