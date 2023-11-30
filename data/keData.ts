2acedfbb9619553d82a11ec566cd9eed5b1e776c
Maxwell <maxwellmwaka3@gmail.com>
2023-11-30 09:08:09 +0300

    Add Maxwell

--- data/keData.ts
+++ data/keData.ts
@@ -1,6 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
 const keData: CountryData[] = [
+    {
+        name: "Maxwell Nzyoka Mwaka",
+        link: "https://github.com/Mwaka3",
+        tags: ["Frontend", "QA"],
+        socials: {
+            github: "Mwaka3",
+            twitter: "",
+            linkedin: "Maxwell Mwaka",
+        },
+    }
 ];
 
 export {keData}
