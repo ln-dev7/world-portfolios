9c6f5bb434197b6de2bbfa351049b68b162df850
Gabin Dongmo <gabin.dongmo@gmail.com>
2023-12-10 13:33:32 -0500

    Refactor to extract SVG assets from NavBar

---
+++ components/icons/ThemeSvg.tsx
@@ -0,0 +1,45 @@
+import { Theme } from "@/contexts/themeContext";
+
+export const ThemeSvg = ({ theme }: { theme: Theme }) => {
+  if (theme === "light") {
+    return (
+      <svg
+        width="46"
+        height="46"
+        fill="none"
+        stroke="#576f7f"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79Z"></path>
+      </svg>
+    );
+  }
+
+  return (
+    <svg
+      width="46"
+      height="46"
+      fill="none"
+      stroke="#576f7f"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 7a5 5 0 1 0 0 10 5 5 0 1 0 0-10z"></path>
+      <path d="M12 1v2"></path>
+      <path d="M12 21v2"></path>
+      <path d="m4.22 4.22 1.42 1.42"></path>
+      <path d="m18.36 18.36 1.42 1.42"></path>
+      <path d="M1 12h2"></path>
+      <path d="M21 12h2"></path>
+      <path d="m4.22 19.78 1.42-1.42"></path>
+      <path d="m18.36 5.64 1.42-1.42"></path>
+    </svg>
+  );
+};
--- contexts/themeContext.tsx
+++ contexts/themeContext.tsx
@@ -1,17 +1,22 @@
-import { createContext, ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
-const localStorageThemeKey = 'world-portfolio-app-theme';
+const localStorageThemeKey = "world-portfolio-app-theme";
+
+export type Theme = "light" | "dark";
 export const ThemeContext = createContext({
-  theme: 'light',
-  toggleTheme: () => {
-  }
+  theme: "light" as Theme,
+  toggleTheme: () => {},
 });
 
-export const ThemeProvider = ({ children }: {
-  children: ReactNode;
-}) => {
-  const savedTheme = localStorage.getItem(localStorageThemeKey);
-  const initialTheme = savedTheme ? savedTheme : 'light';
+export const ThemeProvider = ({ children }: PropsWithChildren) => {
+  const savedTheme = localStorage.getItem(localStorageThemeKey) as Theme;
+  const initialTheme = savedTheme ? savedTheme : "light";
   const [theme, setTheme] = useState(initialTheme);
   const bodyElement = useRef<HTMLElement>(null!);
 
@@ -23,18 +28,21 @@ export const ThemeProvider = ({ children }: {
   }, []);
 
   useEffect(() => {
-    if (theme === 'light') {
-      bodyElement.current.classList.remove('dark');
-      bodyElement.current.classList.add('light');
+    if (theme === "light") {
+      bodyElement.current.classList.remove("dark");
+      bodyElement.current.classList.add("light");
     } else {
-      bodyElement.current.classList.remove('light');
-      bodyElement.current.classList.add('dark');
+      bodyElement.current.classList.remove("light");
+      bodyElement.current.classList.add("dark");
     }
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-    localStorage.setItem(localStorageThemeKey, theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
+    localStorage.setItem(
+      localStorageThemeKey,
+      theme === "light" ? "dark" : "light",
+    );
   };
 
   return (
