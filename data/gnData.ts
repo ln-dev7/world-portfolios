82ca754fde440212da07e882c2853ab95b4cf53c
issa2580 <issadiop2580@gmail.com>
2023-12-12 03:26:03 +0000

    add missing country guinea

---
+++ data/gnData.ts
@@ -0,0 +1,16 @@
+import type CountryData from "@/interfaces/countryData.interface";
+
+const gnData: CountryData[] = [
+  {
+    name: "Issa Diop",
+    link: "https://resumev1.vercel.app/",
+    tags: ["Full Stack"],
+    socials: {
+      github: "issa2580",
+      twitter: "Rootkit221",
+      linkedin: "issa-diop-94kimd08/",
+    },
+  },
+];
+
+export {gnData}
