5617e2defd90d05a8eb228bdd0125d80fdb9b2d9
LN <leonelngoya@gmail.com>
2024-02-17 17:02:07 +0100

    add Christopher Odhiambo

--- data/keData.ts
+++ data/keData.ts
@@ -1,16 +1,26 @@
 import type CountryData from "@/interfaces/countryData.interface";
 
 const keData: CountryData[] = [
-    {
-        name: "Maxwell Nzyoka Mwaka",
-        link: "https://github.com/Mwaka3",
-        tags: ["Frontend", "QA"],
-        socials: {
-            github: "Mwaka3",
-            twitter: "",
-            linkedin: "maxwell-mwaka-bb9a82230",
-        },
-    }
+  {
+    name: "Maxwell Nzyoka Mwaka",
+    link: "https://github.com/Mwaka3",
+    tags: ["Frontend", "QA"],
+    socials: {
+      github: "Mwaka3",
+      twitter: "",
+      linkedin: "maxwell-mwaka-bb9a82230",
+    },
+  },
+  {
+    name: "Christopher Odhiambo",
+    link: "https://the-coding-montana.vercel.app",
+    tags: ["FrontEnd"],
+    socials: {
+      twitter: "",
+      github: "",
+      linkedin: "",
+    },
+  },
 ];
 
-export {keData}
+export { keData };
--- helpers/countries.ts
+++ helpers/countries.ts
@@ -25,134 +25,134 @@ const countries: Country[] = [
     code: "cm",
     numberCode: "237",
     flag: "/assets/images/cm/flag.png",
-    portfolios: cmData
+    portfolios: cmData,
   },
   {
     name: "Togo",
     code: "tg",
     numberCode: "228",
     flag: "/assets/images/tg/flag.png",
-    portfolios: tgData
+    portfolios: tgData,
   },
   {
     name: "Benin",
     code: "bj",
     numberCode: "229",
     flag: "/assets/images/bj/flag.png",
-    portfolios: bjData
+    portfolios: bjData,
   },
   {
     name: "Senegal",
     code: "sn",
     numberCode: "221",
     flag: "/assets/images/sn/flag.png",
-    portfolios: snData
+    portfolios: snData,
   },
   {
     name: "Guinea",
     code: "gn",
     numberCode: "224",
     flag: "/assets/images/gn/flag.png",
-    portfolios: gnData
+    portfolios: gnData,
   },
   {
     name: "Côte d'Ivoire",
     code: "ci",
     numberCode: "225",
     flag: "/assets/images/ci/flag.png",
-    portfolios: ciData
+    portfolios: ciData,
   },
   {
     name: "France",
     code: "fr",
     numberCode: "33",
     flag: "/assets/images/fr/flag.png",
-    portfolios: frData
+    portfolios: frData,
   },
   {
     name: "South Korea",
     code: "kr",
     numberCode: "410",
     flag: "/assets/images/kr/flag.png",
-    portfolios: krData
+    portfolios: krData,
   },
   {
     name: "USA",
     code: "us",
     numberCode: "840",
     flag: "/assets/images/us/flag.png",
-    portfolios: usData
+    portfolios: usData,
   },
   {
     name: "India",
     code: "in",
     numberCode: "91",
     flag: "/assets/images/in/flag.png",
-    portfolios: inData
+    portfolios: inData,
   },
   {
     name: "République démocratique du Congo",
     code: "cd",
     numberCode: "243",
     flag: "/assets/images/cd/flag.png",
-    portfolios: cdData
+    portfolios: cdData,
   },
   {
     name: "Indonesia",
     code: "id",
     numberCode: "62",
     flag: "/assets/images/id/flag.png",
-    portfolios: idData
+    portfolios: idData,
   },
   {
     name: "Brazil",
     code: "br",
     numberCode: "55",
     flag: "/assets/images/br/flag.png",
-    portfolios: brData
+    portfolios: brData,
   },
   {
     name: "Gabon",
     code: "ga",
     numberCode: "241",
     flag: "/assets/images/ga/flag.png",
-    portfolios: gaData
+    portfolios: gaData,
   },
   {
     name: "Myanmar (Burma)",
     code: "mm",
     numberCode: "95",
     flag: "/assets/images/mm/flag.png",
-    portfolios: mmData
+    portfolios: mmData,
   },
   {
     name: "Kenya",
     code: "ke",
     numberCode: "254",
     flag: "/assets/images/ke/flag.png",
-    portfolios: keData
+    portfolios: keData,
   },
   {
     name: "Congo-Brazzaville",
     code: "cg",
     numberCode: "242",
     flag: "/assets/images/cg/flag.png",
-    portfolios: cgData
+    portfolios: cgData,
   },
   {
     name: "Türkiye",
     code: "tr",
     numberCode: "90",
     flag: "/assets/images/tr/flag.png",
-    portfolios: trData
+    portfolios: trData,
   },
   {
     name: "Angola",
     code: "ao",
     numberCode: "244",
     flag: "/assets/images/ao/flag.png",
-    portfolios: aoData
+    portfolios: aoData,
   },
 ];
 
-export default countries.sort((a, b) => a.name.localeCompare(b.name))
+export default countries.sort((a, b) => a.name.localeCompare(b.name));
