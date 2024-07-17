94d53d2d7a70959d16d1b0638199476fcddf86ae
Loïk É. Mallat <lemallat@outlook.com>
2024-07-17 19:39:29 -0400

    feat: add loom4k's portfolio

--- data/caData.ts
+++ data/caData.ts
@@ -1,5 +1,16 @@
 import type CountryData from "@/interfaces/countryData.interface.ts";
 
-const caData: CountryData[] = [];
+const caData: CountryData[] = [
+    {
+        name: "Loïk É. Mallat",
+        link: "https://www.loom4k.me",
+        tags: ["FrontEnd", "Backend", "System Architecture"],
+        socials: {
+            twitter: "loom4kdoescode",
+            github: "loom4k",
+            linkedin: "lemallat",
+        },
+    }
+];
 
 export {caData};
